261a06ffde0d56e788fca7ad242cc8bb893210d7
VishwanathAsundi <itzme.vishwanath.asundi@gmail.com>
2021-11-23 17:22:44 +0530

    server side validation with express validator

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,13 +1,29 @@
+const { validationResult } = require("express-validator/check");
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({ posts: [{ id: "hey" }] });
 };
 
 exports.createPost = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    res
+      .status(422)
+      .json({ message: "Unprocessible entity", errors: errors.array() });
+  }
   const title = req.body.title;
   const content = req.body.content;
 
   res.status(201).json({
     message: "Post created successfully",
-    post: { id: new Date().toISOString(), title, content }
+    post: {
+      _id: new Date().toISOString(),
+      title,
+      content,
+      creator: {
+        name: "vish"
+      },
+      createdAt: new Date()
+    }
   });
 };
--- routes/feed.js
+++ routes/feed.js
@@ -1,11 +1,23 @@
 const express = require("express");
 
 const feedController = require("../controllers/feed");
+const { body } = require("express-validator/check");
 
 const feedRoutes = express.Router();
 
 feedRoutes.get("/posts", feedController.getPosts);
 
-feedRoutes.post("/post", feedController.createPost);
+feedRoutes.post(
+  "/post",
+  [
+    body("title")
+      .trim()
+      .isLength({ min: 5 }),
+    body("content")
+      .trim()
+      .isLength({ min: 5 })
+  ],
+  feedController.createPost
+);
 
 module.exports = feedRoutes;
